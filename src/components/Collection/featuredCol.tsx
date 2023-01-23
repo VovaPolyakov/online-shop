e446e8afebff7ffcf081a4e0be4f8aefa6651783
Vladimir <1337vovalenovo@gmail.com>
2023-01-23 10:34:03 +0300

    all work

--- src/components/Collection/featuredCol.tsx
+++ src/components/Collection/featuredCol.tsx
@@ -1,8 +1,18 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import styles from '../../styles/Home.module.scss'
+import { Products } from '@prisma/client'
+import Image from 'next/image'
 
-const Page = () => {
+
+type Props = {
+  data: Products[]
+}
+
+
+const Page = ({data}:Props) => {
+  console.log(data[0].image_url)
+  const image = 'https://ibb.co/1GjPgHY'
   return (
     <section className={styles.collection}>
       <Head>
@@ -15,7 +25,7 @@ const Page = () => {
           <h1 className={styles.title}>Featured Collection</h1>
           <Link href='/collection/hoodies'>
             <div className={styles.hoodie}>
-              {/* <Image priority  src={Products.image_url} alt='hoodie'></Image> */}
+              <img className={styles.hoodie__img}  src='https://i.ibb.co/nMtYGhq/hoodie.webp' alt='hoodie'></img>
               <p className={styles.text}>Winter Hoodie Collections
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className={styles.arrow} viewBox="0 0 16 16">
                   <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,14 +1,14 @@
-import { Products } from '@prisma/client'
 import { GetStaticProps, InferGetStaticPropsType } from 'next'
 import prisma from '../../prisma/Client'
+import { Products } from '@prisma/client'
+
 
 export const getServerSideProps: GetStaticProps<{ products: Products[]}> = async (ctx) => {
-  const where = undefined // products filter
-  const take = 10 // items on a page
-  const products = await prisma.products.findMany({ where, take })
+
+  const products = await prisma.products.findMany()
   return { props:{ products } }
 }
-
+ 
 export default function Page({products}: InferGetStaticPropsType<typeof getServerSideProps>) {
   return <ul>
     {products.map(({name}) => <li>{name}</li>)}
